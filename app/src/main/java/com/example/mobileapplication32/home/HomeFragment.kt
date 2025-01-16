52db772be0f2f7ac008977605394bad9dc7b24e6
mikheil-cmd <152980324+mikheil-cmd@users.noreply.github.com>
2025-01-16 10:28:50 +0400

    HomeFragment.kt

--- app/src/main/java/com/example/mobileapplication32/home/HomeFragment.kt
+++ app/src/main/java/com/example/mobileapplication32/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mobileapplication32.R
 import com.example.mobileapplication32.databinding.FragmentHomeBinding
 
@@ -16,8 +17,17 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        setupViewPager()
+
         return binding.root
+    }
+
+    private fun setupViewPager() {
+        val adapter = HomePagerAdapter(parentFragmentManager, lifecycle)
+
+        binding.viewPager.adapter = adapter
     }
 
     companion object {
